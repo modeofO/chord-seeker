dd246f0f730c5089c6b33ab318d5fcba4b6cb4c2
ModeofO <mattjtollie@gmail.com>
2026-01-16 14:31:24 -0700

    adding more tunings

--- src/data/tunings.ts
+++ src/data/tunings.ts
@@ -45,6 +45,36 @@ export const TUNING_PRESETS: Record<string, TuningDefinition> = {
     id: 'openC',
     name: 'Open C (C G C G C E)',
     notes: ['C', 'G', 'C', 'G', 'C', 'E']
+  },
+  dropCSharp: {
+    id: 'dropCSharp',
+    name: 'Drop C# (C# G# C# F# A# D#)',
+    notes: ['C#', 'G#', 'C#', 'F#', 'A#', 'D#']
+  },
+  cSharpStandardLowB: {
+    id: 'cSharpStandardLowB',
+    name: 'C# Standard Low B (C# F# B E G# B)',
+    notes: ['C#', 'F#', 'B', 'E', 'G#', 'B']
+  },
+  dropB: {
+    id: 'dropB',
+    name: 'Drop B (B F# B E G# C#)',
+    notes: ['B', 'F#', 'B', 'E', 'G#', 'C#']
+  },
+  dropA: {
+    id: 'dropA',
+    name: 'Drop A (A E A D F# B)',
+    notes: ['A', 'E', 'A', 'D', 'F#', 'B']
+  },
+  openE: {
+    id: 'openE',
+    name: 'Open E (E B E G# B E)',
+    notes: ['E', 'B', 'E', 'G#', 'B', 'E']
+  },
+  openA: {
+    id: 'openA',
+    name: 'Open A (E A E A C# E)',
+    notes: ['E', 'A', 'E', 'A', 'C#', 'E']
   }
 }
 
